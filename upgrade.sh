8e258a3272e824224ff928543f62eb9ad17c2076
aimuch <liuvay@gmail.com>
2019-06-22 23:07:04 +0800

    init

---
+++ upgrade.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Program:
+#           Thie program upgrade linux software.
+# History:
+#           22/06/2019  Andy  First Realse
+
+sudo apt-get update
+sudo apt-get upgrade
+
+echo "Done!"
+exit(0)
