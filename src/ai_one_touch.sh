68d3e66f856ca92de2bbd575f73516223964b1cb
aimuch <liuvay@gmail.com>
2019-06-23 12:25:19 +0800

    create AI one touch

---
+++ src/ai_one_touch.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+# Author: Andy Liu
+# Program:
+#   The program install ai envirnment in one touch.
+# History:
+#   23/06/2019  Andy  First Realse
+
+# NVIDIA Driver
--- src/upgrade_softeare.sh
+++ src/upgrade_softeare.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # Program:
-#           Thie program upgrade linux software.
+#   This program upgrade linux software.
 # History:
-#           22/06/2019  Andy  First Realse
+#   22/06/2019  Andy  First Realse
 
 sudo apt-get update
 sudo apt-get upgrade
